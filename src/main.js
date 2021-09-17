cf251e8d186a028e48a15ac90ace813785099381
sarah boo <srhboo@gmail.com>
2021-09-17 00:57:23 -0400

    make rotatable parts

--- src/main.js
+++ src/main.js
@@ -6,16 +6,18 @@ import { basicData } from "./data/basic";
 import { CSVToArray } from "./utils/csv";
 import { random } from "./utils/misc";
 import { OrbitControls } from "./jsm/controls/OrbitControls";
+import { noise } from "./utils/perlin";
 
-let container, stats, controls;
+let container, stats, controls, clock;
 let camera, scene, raycaster, renderer, parentTransform, sphereInter;
 
 //data
-let bottom = -1000;
+let bottomY = -500;
 let lines = [];
 const points = [];
-const curves = [];
-const curves2 = [];
+const curvePoints = [];
+const curvePoints2 = [];
+let curves = [];
 const matchPoints = [];
 const matchPoints2 = [];
 const tensionPoints = [];
@@ -36,6 +38,7 @@ init();
 animate();
 
 function init() {
+  clock = new THREE.Clock();
   container = document.createElement("div");
   document.body.appendChild(container);
 
@@ -76,25 +79,8 @@ function init() {
   parentTransform = new THREE.Object3D();
 
   drawGrid(parentTransform, 10);
-
-  drawGrid(parentTransform, 500, 500);
-  const curveMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
-  for (let i = 0; i < curves.length; i++) {
-    const [c1x, c1y, p1x, p1y, p2x, p2y, c2x, c2y] = curves[i];
-
-    const curve = new THREE.CubicBezierCurve3(
-      new THREE.Vector3(p1x, p1y, 10),
-      new THREE.Vector3(c1x, c1y, random(-500, 500)),
-      new THREE.Vector3(c2x, c2y, random(-500, 500)),
-      new THREE.Vector3(p2x, p2y, 10)
-    );
-    const points = curve.getPoints(50);
-
-    const curveGeometry = new THREE.BufferGeometry().setFromPoints(points);
-
-    const curveObject = new THREE.Line(curveGeometry, curveMaterial);
-    parentTransform.add(curveObject);
-  }
+  drawCurves(parentTransform);
+  relaxCurves();
   scene.add(parentTransform);
 
   document.addEventListener("pointermove", onPointerMove);
@@ -116,6 +102,72 @@ function onPointerMove(event) {
   pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
+function drawCurves(parent) {
+  const curveMaterial = new THREE.LineBasicMaterial({
+    color: 0xff0000,
+    linewidth: 1,
+  });
+  for (let i = 0; i < curvePoints.length; i++) {
+    const [c1x, c1y, c1z, p1x, p1y, p1z, p2x, p2y, p2z, c2x, c2y, c2z] =
+      curvePoints[i];
+
+    console.log(c1x, c1y, c1z, p1x, p1y, p1z, p2x, p2y, p2z, c2x, c2y, c2z);
+    const curve = new THREE.CubicBezierCurve3(
+      new THREE.Vector3(p1x, p1y, p1z),
+      new THREE.Vector3(c1x, c1y, c1z),
+      new THREE.Vector3(c2x, c2y, c2z),
+      new THREE.Vector3(p2x, p2y, p2z)
+    );
+    const points = curve.getPoints(50);
+
+    const curveGeometry = new THREE.BufferGeometry().setFromPoints(points);
+
+    const curveObject = new THREE.Line(curveGeometry, curveMaterial);
+    curves.push(curveObject);
+    parent.add(curveObject);
+  }
+}
+
+function relaxCurves() {
+  const delta = clock.getDelta();
+  const time = clock.getElapsedTime() * 10;
+  for (let i = 0; i < curves.length; i++) {
+    const curve = curves[i];
+    const position = curve.geometry.attributes.position;
+
+    const positionBase = curvePoints[i];
+    const [c1x, c1y, c1z, p1x, p1y, p1z, p2x, p2y, p2z, c2x, c2y, c2z] =
+      positionBase;
+
+    const newC1x =
+      c1x +
+      Math.sin(time / 20) +
+      noise.simplex3(p1x / 200, p1y / 200, time / 200) * 5;
+    const newC2y =
+      c2y +
+      Math.sin(time / 20) +
+      noise.simplex3(p2x / 200, p2y / 2000, time / 200) * 5;
+
+    curvePoints[i][0] = newC1x;
+    curvePoints[i][10] = newC2y;
+
+    const newCurve = new THREE.CubicBezierCurve3(
+      new THREE.Vector3(p1x, p1y, p1z),
+      new THREE.Vector3(newC1x, c1y, c1z),
+      new THREE.Vector3(c2x, newC2y, c2z),
+      new THREE.Vector3(p2x, p2y, p2z)
+    );
+    const points = newCurve.getPoints(50);
+    for (let i = 0; i < position.count; i++) {
+      position.setX(i, points[i].x);
+      position.setY(i, points[i].y);
+      position.setZ(i, points[i].z);
+    }
+    position.needsUpdate = true;
+    curve.geometry.computeBoundingBox();
+  }
+}
+
 //
 
 function animate() {
@@ -123,6 +175,7 @@ function animate() {
 
   render();
   stats.update();
+  relaxCurves();
 }
 
 function render() {
@@ -167,7 +220,7 @@ function setupData() {
   }
 }
 
-let z = 100;
+let z = 0;
 function setupLines() {
   const days = [
     {
@@ -200,70 +253,162 @@ function setupLines() {
     },
   ];
 
+  // set 1
   const vertLines = [];
   for (let i = 0; i < days.length; i++) {
     const { bottomX, type } = days[i];
     const topX = type === "weekday" ? topX1 : topX2;
-    const l = [topX, topY1, bottomX, bottom];
+    const topY = type === "weekday" ? topY1 : topY2;
+    const l = [topX, topY, bottomX, bottomY];
     const lm = (l[3] - l[1]) / (l[2] - l[0]);
     const lb = l[1] - lm * l[0];
-    lines.push(l);
-    vertLines.push({ m: lm, b: lb });
+    lines.push({ l, plane: "xy" });
+    vertLines.push({ m: lm, b: lb, x: topX });
   }
   const horiLines = [];
 
   // horizontal lines
-  for (let i = 0; i < weeks; i++) {
+  const phase1 = 7;
+  for (let i = 0; i < phase1; i++) {
     const lHorY = -200 - i * 50;
     const l1 = vertLines[0];
     const l7 = vertLines[vertLines.length - 1];
     const lHor = [(lHorY - l1.b) / l1.m, lHorY, (lHorY - l7.b) / l7.m, lHorY];
     horiLines.push(lHorY);
-    lines.push(lHor);
+    lines.push({ l: lHor, plane: "xy" });
   }
   for (let j = 0; j < horiLines.length; j++) {
     for (let i = 0; i < vertLines.length; i++) {
       const { m, b } = vertLines[i];
       const y = horiLines[j];
       const x = (y - b) / m;
-      points.push({ x, y });
+      points.push({ x, y, z: 0 });
+    }
+  }
+  // end set 1
+
+  // set 2
+  const vertLines2 = [];
+  const bottomY2 = -1000;
+  const lastRow = points.slice(points.length - 7, points.length);
+  for (let i = 0; i < days.length; i++) {
+    const { bottomX, type } = days[i];
+    const topX = lastRow[i].x;
+    const topY = lastRow[i].y;
+    const l = [topX, topY, bottomX, bottomY2];
+    const lm = (l[3] - l[1]) / (l[2] - l[0]);
+    const lb = l[1] - lm * l[0];
+    lines.push({ l, plane: "xz" });
+    vertLines2.push({ m: lm, b: lb, x: topX });
+  }
+
+  const horiLines2 = [];
+  const phase2 = 50;
+  // // horizontal lines
+  for (let i = 0; i < phase2; i++) {
+    const lHorY = -200 - i * 50;
+    const l1 = vertLines2[0];
+    const l7 = vertLines2[vertLines2.length - 1];
+    const l1Fin = isFinite(l1.m);
+    const l7Fin = isFinite(l7.m);
+    const lHor = [
+      l1Fin ? (lHorY - l1.b) / l1.m : l1.x,
+      lHorY,
+      l7Fin ? (lHorY - l7.b) / l7.m : l7.x,
+      lHorY,
+    ];
+    horiLines2.push(lHorY);
+    lines.push({ l: lHor, plane: "xz" });
+  }
+  for (let j = 0; j < horiLines2.length; j++) {
+    for (let i = 0; i < vertLines2.length; i++) {
+      const { m, b } = vertLines2[i];
+      const y = horiLines2[j];
+      const x = (y - b) / m;
+      points.push({ x, y, z });
     }
   }
 
+  // POINTS START
   for (let i = 0; i < matchPoints.length - 1; i++) {
     const px1 = random(-900, 1000);
     const py1 = random(-900, 1000);
+    const pz1 = random(-900, 1000);
     const px2 = random(-900, 1000);
     const py2 = random(-900, 1000);
+    const pz2 = random(-900, 1000);
 
     const p3 = getCoordFromSet(matchPoints[i]);
     const p4 = getCoordFromSet(matchPoints[i + 1]);
 
-    curves.push([px1, py1, p3.x, p3.y, p4.x, p4.y, px2, py2]);
+    curvePoints.push([
+      px1,
+      py1,
+      pz1,
+      p3.x,
+      p3.y,
+      p3.z,
+      p4.x,
+      p4.y,
+      p4.z,
+      px2,
+      py2,
+      pz2,
+    ]);
   }
 
-  for (let i = 0; i < matchPoints2.length - 1; i++) {
-    const px1 = random(-900, 1000);
-    const py1 = random(-900, 1000);
-    const px2 = random(-900, 1000);
-    const py2 = random(-900, 1000);
+  // for (let i = 0; i < matchPoints2.length - 1; i++) {
+  //   const px1 = random(-900, 1000);
+  //   const py1 = random(-900, 1000);
+  //   const pz1 = random(-900, 1000);
+  //   const px2 = random(-900, 1000);
+  //   const py2 = random(-900, 1000);
+  //   const pz2 = random(-900, 1000);
 
-    const p3 = getCoordFromSet(matchPoints2[i]);
-    const p4 = getCoordFromSet(matchPoints2[i + 1]);
+  //   const p3 = getCoordFromSet(matchPoints2[i]);
+  //   const p4 = getCoordFromSet(matchPoints2[i + 1]);
 
-    curves.push([px1, py1, p3.x, p3.y, p4.x, p4.y, px2, py2]);
-  }
-  for (let i = 0; i < tensionPoints.length - 2; i++) {
-    const px1 = random(-900, 2000);
-    const py1 = random(-900, 2000);
-    const px2 = random(-900, 2000);
-    const py2 = random(-900, 2000);
+  //   curvePoints.push([
+  //     px1,
+  //     py1,
+  //     pz1,
+  //     p3.x,
+  //     p3.y,
+  //     p3.z,
+  //     p4.x,
+  //     p4.y,
+  //     p4.z,
+  //     px2,
+  //     py2,
+  //     pz2,
+  //   ]);
+  // }
+  // for (let i = 0; i < tensionPoints.length - 2; i++) {
+  //   const px1 = random(-900, 1000);
+  //   const py1 = random(-900, 1000);
+  //   const pz1 = random(-900, 1000);
+  //   const px2 = random(-900, 1000);
+  //   const py2 = random(-900, 1000);
+  //   const pz2 = random(-900, 1000);
 
-    const p3 = getCoordFromSet(tensionPoints[i]);
-    const p4 = getCoordFromSet(tensionPoints[i + 1]);
+  //   const p3 = getCoordFromSet(tensionPoints[i]);
+  //   const p4 = getCoordFromSet(tensionPoints[i + 1]);
 
-    curves2.push([px1, py1, p3.x, p3.y, p4.x, p4.y, px2, py2]);
-  }
+  //   curvePoints2.push([
+  //     px1,
+  //     py1,
+  //     pz1,
+  //     p3.x,
+  //     p3.y,
+  //     p3.z,
+  //     p4.x,
+  //     p4.y,
+  //     p4.z,
+  //     px2,
+  //     py2,
+  //     pz2,
+  //   ]);
+  // }
 }
 
 function getCoordFromSet(set) {
@@ -275,10 +420,16 @@ function getCoordFromSet(set) {
 function drawGrid(parent, z, topOffset = 0) {
   const lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
   for (let i = 0; i < lines.length; i++) {
-    const [x1, y1, x2, y2] = lines[i];
+    const { l, plane } = lines[i];
+    const [x1, y1, x2, y2] = l;
     const points = [];
-    points.push(new THREE.Vector3(x1, y1 + topOffset, z));
-    points.push(new THREE.Vector3(x2, y2 + topOffset, z));
+    if (plane == "xy") {
+      points.push(new THREE.Vector3(x1, y1, z));
+      points.push(new THREE.Vector3(x2, y2, z));
+    } else if (plane == "xz") {
+      points.push(new THREE.Vector3(x1, -500, y1));
+      points.push(new THREE.Vector3(x2, -500, y2));
+    }
 
     const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
