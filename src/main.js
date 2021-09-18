b63eec31bca589b6248a862f845eaf3868fe6eb3
sarah boo <srhboo@gmail.com>
2021-09-18 01:20:16 -0400

    randomize

--- src/main.js
+++ src/main.js
@@ -24,10 +24,7 @@ const tensionPoints = [];
 const drinkPoints = [];
 const searchTerm = "tired";
 const searchTerm2 = "energy";
-let topX1 = -200;
-let topY1 = 100;
-let topX2 = 200;
-let topY2 = 100;
+
 let theta = 0;
 let weeks = 0;
 let conDat = [];
@@ -78,7 +75,8 @@ function init() {
   scene.add(sphereInter);
   parentTransform = new THREE.Object3D();
 
-  drawGrid(parentTransform, 10);
+  drawGrid(parentTransform, 0);
+
   drawCurves(parentTransform);
   relaxCurves();
   scene.add(parentTransform);
@@ -111,7 +109,6 @@ function drawCurves(parent) {
     const [c1x, c1y, c1z, p1x, p1y, p1z, p2x, p2y, p2z, c2x, c2y, c2z] =
       curvePoints[i];
 
-    console.log(c1x, c1y, c1z, p1x, p1y, p1z, p2x, p2y, p2z, c2x, c2y, c2z);
     const curve = new THREE.CubicBezierCurve3(
       new THREE.Vector3(p1x, p1y, p1z),
       new THREE.Vector3(c1x, c1y, c1z),
@@ -220,41 +217,53 @@ function setupData() {
   }
 }
 
-let z = 0;
-function setupLines() {
+function generateTops() {
+  let topX1 = random(-400, 200);
+  let topY1 = random(100, 400);
+  let topX2 = random(250, 450);
+  let topY2 = random(100, 400);
+  return { topX1, topY1, topX2, topY2 };
+}
+
+function generateDays() {
   const days = [
     {
-      bottomX: 100,
+      bottomX: random(0, 100),
       type: "weekday",
     },
     {
-      bottomX: 430,
+      bottomX: random(351, 450),
       type: "weekday",
     },
     {
-      bottomX: 180,
+      bottomX: random(101, 180),
       type: "weekday",
     },
     {
-      bottomX: 200,
+      bottomX: random(181, 200),
       type: "weekday",
     },
     {
-      bottomX: 250,
+      bottomX: random(201, 250),
       type: "weekday",
     },
     {
-      bottomX: 350,
+      bottomX: random(251, 350),
       type: "weekend",
     },
     {
-      bottomX: 450,
+      bottomX: random(451, 500),
       type: "weekend",
     },
   ];
+  return days;
+}
 
-  // set 1
+function setupTopVertLines() {
+  const days = generateDays();
   const vertLines = [];
+  const { topX1, topY1, topX2, topY2 } = generateTops();
+
   for (let i = 0; i < days.length; i++) {
     const { bottomX, type } = days[i];
     const topX = type === "weekday" ? topX1 : topX2;
@@ -265,50 +274,44 @@ function setupLines() {
     lines.push({ l, plane: "xy" });
     vertLines.push({ m: lm, b: lb, x: topX });
   }
-  const horiLines = [];
+  return vertLines;
+}
 
-  // horizontal lines
-  const phase1 = 7;
-  for (let i = 0; i < phase1; i++) {
-    const lHorY = -200 - i * 50;
-    const l1 = vertLines[0];
-    const l7 = vertLines[vertLines.length - 1];
-    const lHor = [(lHorY - l1.b) / l1.m, lHorY, (lHorY - l7.b) / l7.m, lHorY];
-    horiLines.push(lHorY);
-    lines.push({ l: lHor, plane: "xy" });
-  }
-  for (let j = 0; j < horiLines.length; j++) {
-    for (let i = 0; i < vertLines.length; i++) {
-      const { m, b } = vertLines[i];
-      const y = horiLines[j];
-      const x = (y - b) / m;
-      points.push({ x, y, z: 0 });
-    }
-  }
-  // end set 1
+function setupVertLines() {
+  const days = generateDays();
 
-  // set 2
-  const vertLines2 = [];
-  const bottomY2 = -1000;
   const lastRow = points.slice(points.length - 7, points.length);
+  const vertLines = [];
+  const bottomY = lastRow[0].y - 1000;
   for (let i = 0; i < days.length; i++) {
-    const { bottomX, type } = days[i];
+    const { bottomX } = days[i];
     const topX = lastRow[i].x;
     const topY = lastRow[i].y;
-    const l = [topX, topY, bottomX, bottomY2];
+    const z = lastRow[i].z;
+    const l = [topX, topY, bottomX, bottomY];
     const lm = (l[3] - l[1]) / (l[2] - l[0]);
     const lb = l[1] - lm * l[0];
-    lines.push({ l, plane: "xz" });
-    vertLines2.push({ m: lm, b: lb, x: topX });
+    lines.push({ l, plane: "xy" });
+    vertLines.push({ m: lm, b: lb, x: topX, z });
+  }
+  return vertLines;
+}
+
+function setupHoriLines(vertLines, rows) {
+  const horiLines = [];
+
+  let currentY = -200;
+
+  if (points.length) {
+    const lastRow = points.slice(points.length - 7, points.length);
+    currentY = lastRow[0].y;
   }
 
-  const horiLines2 = [];
-  const phase2 = 50;
-  // // horizontal lines
-  for (let i = 0; i < phase2; i++) {
-    const lHorY = -200 - i * 50;
-    const l1 = vertLines2[0];
-    const l7 = vertLines2[vertLines2.length - 1];
+  // horizontal lines
+  for (let i = 0; i < rows; i++) {
+    const lHorY = currentY - i * 50;
+    const l1 = vertLines[0];
+    const l7 = vertLines[vertLines.length - 1];
     const l1Fin = isFinite(l1.m);
     const l7Fin = isFinite(l7.m);
     const lHor = [
@@ -317,17 +320,89 @@ function setupLines() {
       l7Fin ? (lHorY - l7.b) / l7.m : l7.x,
       lHorY,
     ];
-    horiLines2.push(lHorY);
-    lines.push({ l: lHor, plane: "xz" });
+    horiLines.push(lHorY);
+    lines.push({ l: lHor, plane: "xy" });
   }
-  for (let j = 0; j < horiLines2.length; j++) {
-    for (let i = 0; i < vertLines2.length; i++) {
-      const { m, b } = vertLines2[i];
-      const y = horiLines2[j];
+  return horiLines;
+}
+
+function setupIntersectionPoints(vertLines, horiLines) {
+  for (let j = 0; j < horiLines.length; j++) {
+    for (let i = 0; i < vertLines.length; i++) {
+      const { m, b } = vertLines[i];
+      const y = horiLines[j];
       const x = (y - b) / m;
-      points.push({ x, y, z });
+      points.push({ x, y, z: 0 });
     }
   }
+}
+
+function setupLines() {
+  // set 1
+
+  let weeksLeft = weeks;
+  const topVertLines = setupTopVertLines();
+  const topRows = random(2, 10);
+  const topHoriLines = setupHoriLines(topVertLines, topRows);
+  setupIntersectionPoints(topVertLines, topHoriLines);
+  weeksLeft -= topRows;
+  while (weeksLeft > 0) {
+    const offset = {
+      x: random(500, 2000),
+      y: random(500, 2000),
+      z: random(500, 2000),
+    };
+    const rows = weeksLeft < 2 ? weeksLeft : random(2, weeksLeft);
+    const vertLines = setupVertLines();
+    const horiLines = setupHoriLines(vertLines, rows);
+    setupIntersectionPoints(vertLines, horiLines);
+    weeksLeft -= rows;
+  }
+
+  // end set 1
+
+  // // set 2
+  // const vertLines2 = [];
+  // const bottomY2 = -2000;
+  // const lastRow = points.slice(points.length - 7, points.length);
+  // for (let i = 0; i < days.length; i++) {
+  //   const { bottomX, type } = days[i];
+  //   const topX = lastRow[i].x;
+  //   const topY = lastRow[i].y;
+  //   const z = lastRow[i].z;
+  //   const l = [topX, topY, bottomX, bottomY2];
+  //   const lm = (l[3] - l[1]) / (l[2] - l[0]);
+  //   const lb = l[1] - lm * l[0];
+  //   lines.push({ l, plane: "xy" });
+  //   vertLines2.push({ m: lm, b: lb, x: topX, z });
+  // }
+
+  // const horiLines2 = [];
+  // const phase2 = 25;
+  // // // horizontal lines
+  // for (let i = 0; i < phase2; i++) {
+  //   const lHorY = -200 - i * 50;
+  //   const l1 = vertLines2[0];
+  //   const l7 = vertLines2[vertLines2.length - 1];
+  // const l1Fin = isFinite(l1.m);
+  // const l7Fin = isFinite(l7.m);
+  //   const lHor = [
+  // l1Fin ? (lHorY - l1.b) / l1.m : l1.x,
+  // lHorY,
+  // l7Fin ? (lHorY - l7.b) / l7.m : l7.x,
+  // lHorY,
+  //   ];
+  //   horiLines2.push(lHorY);
+  //   lines.push({ l: lHor, plane: "xy" });
+  // }
+  // for (let j = 0; j < horiLines2.length; j++) {
+  //   for (let i = 0; i < vertLines2.length; i++) {
+  //     const { m, b, x } = vertLines2[i];
+  //     const y = horiLines2[j];
+  //     const xf = isNaN((y - b) / m) ? x : (y - b) / m;
+  //     points.push({ x: xf, y, z: 0 });
+  //   }
+  // }
 
   // POINTS START
   for (let i = 0; i < matchPoints.length - 1; i++) {
@@ -357,32 +432,32 @@ function setupLines() {
     ]);
   }
 
-  // for (let i = 0; i < matchPoints2.length - 1; i++) {
-  //   const px1 = random(-900, 1000);
-  //   const py1 = random(-900, 1000);
-  //   const pz1 = random(-900, 1000);
-  //   const px2 = random(-900, 1000);
-  //   const py2 = random(-900, 1000);
-  //   const pz2 = random(-900, 1000);
+  for (let i = 0; i < matchPoints2.length - 1; i++) {
+    const px1 = random(-900, 1000);
+    const py1 = random(-900, 1000);
+    const pz1 = random(-900, 1000);
+    const px2 = random(-900, 1000);
+    const py2 = random(-900, 1000);
+    const pz2 = random(-900, 1000);
 
-  //   const p3 = getCoordFromSet(matchPoints2[i]);
-  //   const p4 = getCoordFromSet(matchPoints2[i + 1]);
+    const p3 = getCoordFromSet(matchPoints2[i]);
+    const p4 = getCoordFromSet(matchPoints2[i + 1]);
 
-  //   curvePoints.push([
-  //     px1,
-  //     py1,
-  //     pz1,
-  //     p3.x,
-  //     p3.y,
-  //     p3.z,
-  //     p4.x,
-  //     p4.y,
-  //     p4.z,
-  //     px2,
-  //     py2,
-  //     pz2,
-  //   ]);
-  // }
+    curvePoints.push([
+      px1,
+      py1,
+      pz1,
+      p3.x,
+      p3.y,
+      p3.z,
+      p4.x,
+      p4.y,
+      p4.z,
+      px2,
+      py2,
+      pz2,
+    ]);
+  }
   // for (let i = 0; i < tensionPoints.length - 2; i++) {
   //   const px1 = random(-900, 1000);
   //   const py1 = random(-900, 1000);
@@ -417,7 +492,8 @@ function getCoordFromSet(set) {
   return points[(week - 1) * 7 + dayOfWeek - 1];
 }
 
-function drawGrid(parent, z, topOffset = 0) {
+function drawGrid(parent) {
+  let z = 0;
   const lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
   for (let i = 0; i < lines.length; i++) {
     const { l, plane } = lines[i];
@@ -426,9 +502,6 @@ function drawGrid(parent, z, topOffset = 0) {
     if (plane == "xy") {
       points.push(new THREE.Vector3(x1, y1, z));
       points.push(new THREE.Vector3(x2, y2, z));
-    } else if (plane == "xz") {
-      points.push(new THREE.Vector3(x1, -500, y1));
-      points.push(new THREE.Vector3(x2, -500, y2));
     }
 
     const geometry = new THREE.BufferGeometry().setFromPoints(points);
